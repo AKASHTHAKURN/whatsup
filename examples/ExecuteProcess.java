3d8c13f3b9ac2bf0f1e02a1a5e6124ebffc7c95d
wolfbitzkim <wolfbitz@daum.net>
2020-05-22 13:58:07 +0900

    Update ExecuteProcess.java

--- examples/ExecuteProcess.java
+++ examples/ExecuteProcess.java
@@ -4,30 +4,27 @@ import java.io.*;
 
 public class ExecuteProcess {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// ProcessBuilder 사용
 
-	}
+	/* 외부 프로그램 실행결과를 출력할 경우 */
+	public static void Cmd() throws IOException {
+		String[] cmd = new String[] {"cmd", "/c", "dir"}; // "cmd" 뒷부분에 "/c"를 넣어주면 dir에 대한 수행 결과를 보여준다.
+		Process process = null;
+		String str = null;
 
-	static String codeConv(String msg) throws IOException {
+		try {
+			process = new ProcessBuilder(cmd).start();
+			BufferedReader stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-		String[] cmd = new String[] {"CODECONV.EXE", msg};
-
-		Process process = new ProcessBuilder(cmd).start();
-
-		BufferedReader stdOut = new BufferedReader( new InputStreamReader(process.getInputStream()) );
-
-		//String str;
-		//while( (str = stdOut.readLine()) != null ) {
-		//    System.out.println(str);
-		//}
+			while((str = stdOut.readLine()) != null) {
+				System.out.println(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}	
 	
-		return stdOut.readLine();
-	}
-
-	/*
-	 * signage.exe 에 스트림을 
-	 */
+	/* 외부 프로그램에서 입력을 받을 경우 */
 	public static void sendSignage(ArrayList<String> list) {
 		
 		Process process = null;
@@ -47,4 +44,20 @@ public class ExecuteProcess {
 		    e.printStackTrace();
 		}
 	}	
+
+	static String codeConv(String msg) throws IOException {
+
+		String[] cmd = new String[] {"CODECONV.EXE", msg};
+
+		Process process = new ProcessBuilder(cmd).start();
+
+		BufferedReader stdOut = new BufferedReader( new InputStreamReader(process.getInputStream()) );
+
+		//String str;
+		//while( (str = stdOut.readLine()) != null ) {
+		//    System.out.println(str);
+		//}
+	
+		return stdOut.readLine();
+	}
 }
