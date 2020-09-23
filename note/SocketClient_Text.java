237a371df7dbd1b09a7dbcbb67984129cbae2e63
wolfbitzkim <wolfbitz@daum.net>
2020-09-23 13:01:47 +0900

    Update SocketClient_Text.java

--- note/SocketClient_Text.java
+++ note/SocketClient_Text.java
@@ -1,35 +1,80 @@
 package socket.text;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
 
-// Text file 송수신
-public class SocketServer_Text {
-
-	public static void main(String[] args) throws IOException, InterruptedException 
-	{
-		ServerSocket listener = new ServerSocket(9876);;
-
-	System.out.println("서버가 연결요청을 기다립니다.");
-
-		Socket socket = listener.accept();
+/*
+	Socket 정리
 	
-	System.out.println(socket);
-	System.out.println(socket.getInetAddress() + "로부터 연결요청이 들어왔습니다.");
+	[read]
+	InputStream in = socket.getInputStream();
+		in.read(buffer, 0, BUF_SIZE);
+	
+	DataInputStream dis = new DataInputStream(socket.getInputStream());
+		dis.readInt();
+		dis.read(buffer, 0, BUF_SIZE);
+		dis.readUTF();
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		br.readLine();
+	
+	[write]
+	// OutputStream -> write byte[] 만 가능 (기본형태)
+	
+	// DataOutputStream -> 다양한 형태로 write 가능
+	
+	// BufferedWriter -> String, char[] 
+	
+	// PrintWriter -> print문 사용가능, BufferedWriter 보다는 이걸 써라
+	
+	OutputStream out = socket.getOutputStream();
+		out.write(b, off, len);
+	
+	DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+		dos.writeUTF(str);
+		dos.write(b, off, len);
+			
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		bw.write(str + "\r\n");
+	
+	PrintWriter pw = new PrintWriter(socket.getOutputStream(), true); // autoflush
+		pw.println(str);
+*/
+
+//
+// Text file 송수신
+//
+public class SocketClient_Text 
+{
+	static String serverIp = "127.0.0.1";
+	static int portNo = 9876;
+
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ParseException, InterruptedException {    
+
+		sendFile("./SEND/sample.txt");
+	}
+	
+	public static void sendFile(String fileName) throws IOException
+	{
+		Socket socket = new Socket(serverIp, 9876);
 		
-		// 소켓으로 부터 스트림을 읽어서 파일에 쓰기	
-		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		BufferedWriter bw = new BufferedWriter(new FileWriter("./RECV/sample.txt"));
+	System.out.println(socket);
+	System.out.println("connected to server IP:"  + socket.getInetAddress() + " Port:" +socket.getPort());
+		
+		// 파일을 읽어서 소켓에 쓰기
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 		
 		String line;
 		while((line = br.readLine()) != null) 
-		{
-			bw.write(line + "\r\n");
-		}
+		{ 
+			bw.write(line + "\r\n");	
+		} 
 		br.close();
 		bw.close();
-		
-	System.out.println("server end");
-    }
+
+	System.out.println("client end");
+	}
 }
